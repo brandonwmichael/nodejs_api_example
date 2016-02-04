a5b1bfd57a65ab9b6b175a7e8a643c8db8941ab5
Brandon Michael <mekel8090@gmail.com>
2016-02-04 07:25:11 -0600

    update 1

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 // BASE SETUP
 // =============================================================================
+var db = require('./db');
 
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+console.log(db.connection());
+mongoose.connect(db.connection(),options); // connect to our database
+var Device     = require('./app/models/device');
+
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,12 +29,49 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/devices')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+
+        var device = new Device();      // create a new instance of the Bear model
+        device.name = req.body.name;  // set the bears name (comes from the request)
+        // save the bear and check for errors
+        device.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Device created!' });
+        });
+
+    })
+
+    // get all the bears (accessed at GET http://localhost:8080/api/bears)
+    .get(function(req, res) {
+        Device.find(function(err, devices) {
+            if (err)
+                res.send(err);
+
+            res.json(devices);
+        });
+    });
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
